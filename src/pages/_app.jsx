18676fd7ca84836de254b006bd296ea440dbebbe
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-06-18 22:44:34 +0900

    🐛 トースト表示ができない不具合を修正

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,13 @@
 import "tailwindcss/tailwind.css";
+import { Toaster } from "react-hot-toast";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster />
+    </>
+  );
 };
 
 export default MyApp;
