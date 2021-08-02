1b8198923e38266c58354055d1f137b71a5c1803
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-08-02 14:46:52 +0900

    ✨ 新規登録時にログイン状態にするように修正

--- src/hooks/useSignUp.js
+++ src/hooks/useSignUp.js
@@ -8,12 +8,41 @@ export const useSignUp = () => {
   const { showNotify } = useNotify();
   const [loading, setLoading] = useState(false);
 
+  // スネークケースからキャメルケースに変換（文字列）.
+  const toCamelCase = (str) => {
+    return str
+      .split("_")
+      .map(function (word, index) {
+        if (index === 0) {
+          return word.toLowerCase();
+        }
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+      })
+      .join("");
+  };
+
+  // スネークケースからキャメルケースに変換（オブジェクト）.
+  const toCamelCaseObject = (obj) => {
+    const result = {};
+    Object.keys(obj).forEach((key) => {
+      result[toCamelCase(key)] = obj[key];
+    });
+    return result;
+  };
+
   const signUp = (signUpData) => {
     setLoading(true);
     axios
       .post(`http://localhost:3001/auth`, signUpData)
       .then((res) => {
         if (res.data) {
+          localStorage.setItem("accessToken", res.headers["access-token"]);
+          localStorage.setItem("client", res.headers["client"]);
+          localStorage.setItem("uid", res.headers["uid"]);
+
+          const resData = toCamelCaseObject(res.data.data);
+          localStorage.setItem("loginId", JSON.stringify(res.data.data.id));
+          localStorage.setItem("loginUser", JSON.stringify(resData));
           showNotify({ title: "登録完了しました", status: "success" });
           router.push("/");
         } else {
