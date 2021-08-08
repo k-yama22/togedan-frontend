f158f84f49ab3505bc9636617b234ec8d62fac34
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-08-08 18:13:56 +0900

    🐛 不要な処理を削除

--- src/hooks/useUserChange.js
+++ src/hooks/useUserChange.js
@@ -3,34 +3,11 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import { authHeaders } from "src/hooks/authHeaders";
 import { useNotify } from "src/hooks/useNotify";
-import lscache from "lscache";
 
 export const useUserChange = () => {
   const router = useRouter();
   const { showNotify } = useNotify();
   const [loading, setLoading] = useState(false);
-
-  // スネークケースからキャメルケースに変換（文字列）.
-  const toCamelCase = (str) => {
-    return str
-      .split("_")
-      .map(function (word, index) {
-        if (index === 0) {
-          return word.toLowerCase();
-        }
-        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-      })
-      .join("");
-  };
-
-  // スネークケースからキャメルケースに変換（オブジェクト）.
-  const toCamelCaseObject = (obj) => {
-    const result = {};
-    Object.keys(obj).forEach((key) => {
-      result[toCamelCase(key)] = obj[key];
-    });
-    return result;
-  };
 
   const userChange = (editData) => {
     setLoading(true);
@@ -39,9 +16,6 @@ export const useUserChange = () => {
       .put(`http://localhost:3001/auth`, editData, { headers: headers })
       .then((res) => {
         if (res.data) {
-          const resData = toCamelCaseObject(res.data.data);
-          lscache.set("loginId", JSON.stringify(res.data.data.id));
-          lscache.set("loginUser", JSON.stringify(resData));
           showNotify({ title: "変更完了しました", status: "success" });
           router.push("/myPage");
         } else {
