20a288cef4e23b26ab5e60c6e8f147f42771648c
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-08-26 22:48:55 +0900

    ✨ 確認モーダルの追加とレイアウト微修正

---
+++ src/components/ConfirmModal.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+
+export const ConfirmModal = (props) => {
+  const { id, buttonMessage, eventName, onClick } = props;
+  const [showModal, setShowModal] = useState(false);
+  const [modalId, setSModalId] = useState();
+
+  return (
+    <>
+      <button
+        className="bg-red-500 text-white active:bg-red-600 font-bold text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => {
+          setShowModal(true);
+          setSModalId(id);
+        }}
+      >
+        {buttonMessage}
+      </button>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="bg-red-200 flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl text-gray-700 font-semibold">
+                    予約申込確認
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-20 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-1 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-black-500 text-lg leading-relaxed">
+                    ■{eventName}
+                    <br />
+                    <br />
+                    上記の内容で予約をしますがよろしいですか？
+                    <br />
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-black-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    閉じる
+                  </button>
+                  <button
+                    className="bg-red-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => {
+                      onClick(modalId);
+                      setShowModal(false);
+                    }}
+                  >
+                    予約する
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+};
+
+export default ConfirmModal;
--- src/pages/eventDetail.jsx
+++ src/pages/eventDetail.jsx
@@ -7,6 +7,7 @@ import { useSelectEvent } from "src/hooks/useSelectEvent";
 import { useNewReserve } from "src/hooks/useNewReserve";
 import dayjs from "dayjs";
 import router from "next/router";
+import ConfirmModal from "src/components/ConfirmModal";
 
 //サーバーサイドレンダリング
 export async function getServerSideProps(context) {
@@ -148,24 +149,16 @@ const EventDetail = (props) => {
                         <div className="title-font font-medium text-2xl text-gray-900">
                           {selectedEvent.location}
                         </div>
-                        <button
-                          className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded"
-                          onClick={onClickReserve}
-                        >
-                          予約申込をする
-                        </button>
-                        <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                          <svg
-                            fill="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            className="w-5 h-5"
-                            viewBox="0 0 24 24"
-                          >
-                            <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                          </svg>
-                        </button>
+                        <div className="flex ml-auto focus:outline-none rounded">
+                          <ConfirmModal
+                            id={selectedEvent.event_id}
+                            buttonMessage={"予約申込をする"}
+                            eventName={selectedEvent.event_name}
+                            onClick={() =>
+                              onClickReserve(selectedEvent.event_id)
+                            }
+                          />
+                        </div>
                       </div>
                     </div>
                   </div>
