e161a8123b20a14f3718ea915d5b64abecf49d47
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-09-09 14:38:42 +0900

    🐛 不具合の修正

--- src/hooks/useUserChange.js
+++ src/hooks/useUserChange.js
@@ -5,9 +5,9 @@ import { authHeaders } from "src/hooks/authHeaders";
 import { useNotify } from "src/hooks/useNotify";
 import lscache from "lscache";
 import {
+  AUTH_URL,
   COMMON_UPDATE_ERROR,
   ERROR_STATUS,
-  SIGNUP_URL,
   SUCCESS_STATUS,
   UPDATE_FAILED,
   UPDATE_SUCCESS,
@@ -22,7 +22,7 @@ export const useUserChange = () => {
     setLoading(true);
     const headers = authHeaders();
     axios
-      .put(SIGNUP_URL, editData, { headers: headers })
+      .put(AUTH_URL, editData, { headers: headers })
       .then((res) => {
         if (res.data) {
           showNotify({ title: UPDATE_SUCCESS, status: SUCCESS_STATUS });
