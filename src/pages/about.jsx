1e2933da93b5a97d2ea9b3dcd0f05b2a6e784529
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-09-17 23:43:26 +0900

    💄 スマホ表示時のレイアウトを変更

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -24,66 +24,153 @@ const About = () => {
                   {/* eslint-disable-next-line @next/next/no-img-element */}
                   <img
                     className="w-full object-cover h-3/4 block mx-auto sm:block sm:w-full"
-                    src="https://source.unsplash.com/z8Tul255kGg/1920x588"
+                    src="https://source.unsplash.com/z8Tul255kGg/1920x988"
                     alt="Banner"
                     width="1920"
-                    height="588"
+                    height="988"
                   />
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="container mx-auto">
-          <h1 className="text-center text-black-800 py-6 text-5xl mt-4 ">
-            サービスについて
-          </h1>
-          <div className="text-gray-900">
-            <div className="grid grid-cols-6 mb-20">
-              <div className="col-span-5">
-                <h2 className="text-gray-700 leading-10 text-3xl">
-                  <p className="text-purple-800 font-bold">
-                    〜好きなダンスをいつでもどこでも誰とでも〜
-                  </p>
-                  TogetherDanceは様々なダンスを楽しみたい！色々な人たちと踊りたい！という人と
-                  <br />
-                  自分の好きなダンスを一緒に踊ってくれる人を見つけたい！一緒に好きなアーティストの曲で踊る仲間を見つけたい！という人をマッチングさせるサービスです！
-                  <br />
-                  誰でも参加する側にも開催する側にもなることができます！
-                  <br />
-                  参加したい人は好きなダンスを踊る会（踊ろう会）を探して好きな会に参加することができます！
-                  <br />
-                  開催したい人は主催者として参加者を募り、自由に踊ろう会を開催することができます！
-                  <br />
-                  自分で振り付けを考えたダンスをみんなに教えながら一緒に踊るもよし！
-                  <br />
-                  同じアーティストが好きな同士を集めてダンスの振りコピを踊るもよし！
-                  <br />
-                  いつでもどこでも誰とでも好きなダンスを楽しむ機会を作り出す！
-                  <br />
-                  それがTogether Danceというサービスです！
-                  <br />
-                  こんな人にぴったり
-                  <br />
-                  好きなダンスを気楽に踊る場が欲しい
-                  <br />
-                  ダンスを一緒に踊る人が見つけたい
-                  <br />
-                  自分で作った振り付けを振り入れしたいけど振り入れできる場がない
-                  <br />
-                  ダンスを教えてみたいけどレッスンを開講する機会はがなかなかない
-                  <br />
-                  そんな方々に使って欲しいと思っています！ぜひ一度使ってみてください！
-                  <br />
-                </h2>
+        <div className="relative ">
+          <div className="sliderAx h-4/5">
+            <div id="slider-1" className="mx-auto">
+              <div className="bg-black">
+                <div className="flex relative text-center">
+                  <div className="text-3xl tracking-wider text-white uppercase font-bold pt-20 md:pt-48 content-center absolute text-center w-full md:text-6xl z-10">
+                    好きなダンスを
+                    <br />
+                    いつでもどこでも誰とでも
+                    <p className="text-sm md:text-5xl p-4 md:pt-48 text-white font-semibold opacity-100">
+                      TogetherDanceは
+                      <div className="font-bold md:py-8">
+                        「好きなダンスを色々な人と踊りたい！」
+                      </div>
+                      という人と
+                      <div className="font-bold md:py-8">
+                        「振付したダンスを踊ってくれる人を見つけたい！」
+                        <br />
+                        「完コピする仲間を見つけたい！」
+                      </div>
+                      という人を結びつけるきっかけを作ります！
+                    </p>
+                  </div>
+                  {/* <div className="opacity-100"> */}
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img
+                    className="w-full object-cover h-3/4 md:h-1/2 block mx-auto sm:block sm:w-full opacity-30 "
+                    src="https://source.unsplash.com/N0g-deioHO4/1920x1920"
+                    alt="Banner"
+                    width="1920"
+                    height="1920"
+                  />
+                  {/* </div> */}
+                </div>
               </div>
             </div>
           </div>
         </div>
+        {/* <div className="container mx-auto"> */}
+
+        <div className="relative ">
+          <div className="sliderAx h-4/5">
+            <div id="slider-1" className="mx-auto">
+              <div className="bg-black">
+                <div className="flex relative text-center">
+                  <div className="text-xs tracking-wider text-white uppercase font-bold pt-20 content-center absolute text-center w-full md:text-4xl z-10">
+                    <p className="text-base md:text-6xl text-white font-bold opacity-100 p-2">
+                      参加も開催も自由
+                    </p>
+                    <div className="text-white font-bold opacity-100">
+                      参加したい人はダンスを踊る会（踊ろう会）を探して参加することができます！
+                      <br />
+                      開催したい人は主催者として参加者を募り、踊ろう会を開催することができます！
+                      <br />
+                      自分で振り付けを考えたダンスをみんなに教えながら一緒に踊るもよし！
+                      <br />
+                      同じアーティストが好きな同士を集めてダンスの振りコピを踊るもよし！
+                      <br />
+                      いつでもどこでも誰とでも好きなダンスを楽しむ機会を作り出す！
+                      <br />
+                      それがTogether Danceというサービスです！
+                      <br />
+                      こんな人にぴったり
+                      <br />
+                      好きなダンスを気楽に踊る場が欲しい
+                      <br />
+                      ダンスを一緒に踊る人が見つけたい
+                      <br />
+                      自分で作った振り付けを振り入れしたいけど振り入れできる場がない
+                      <br />
+                      ダンスを教えてみたいけどレッスンを開講する機会はがなかなかない
+                      <br />
+                      そんな方々に使って欲しいと思っています！ぜひ一度使ってみてください！
+                      <br />
+                    </div>
+                  </div>
+                  {/* <div className="opacity-100"> */}
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img
+                    className="w-full object-cover h-3/4 block mx-auto sm:block sm:w-full opacity-30 "
+                    src="https://source.unsplash.com/3v3EMQI3pYQ/4000x5000"
+                    alt="Banner"
+                    width="4000"
+                    height="5000"
+                  />
+                  {/* </div> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className="text-gray-900">
+          <div className="grid grid-cols-6 mb-20">
+            <div className="col-span-5">
+              <h2 className="text-gray-700 leading-10 text-3xl">
+                <p className="text-purple-800 font-bold">
+                  〜好きなダンスをいつでもどこでも誰とでも〜
+                </p>
+                TogetherDanceは様々なダンスを楽しみたい！色々な人たちと踊りたい！という人と
+                <br />
+                自分の好きなダンスを一緒に踊ってくれる人を見つけたい！一緒に好きなアーティストの曲で踊る仲間を見つけたい！という人をマッチングさせるサービスです！
+                <br />
+                誰でも参加する側にも開催する側にもなることができます！
+                <br />
+                参加したい人は好きなダンスを踊る会（踊ろう会）を探して好きな会に参加することができます！
+                <br />
+                開催したい人は主催者として参加者を募り、自由に踊ろう会を開催することができます！
+                <br />
+                自分で振り付けを考えたダンスをみんなに教えながら一緒に踊るもよし！
+                <br />
+                同じアーティストが好きな同士を集めてダンスの振りコピを踊るもよし！
+                <br />
+                いつでもどこでも誰とでも好きなダンスを楽しむ機会を作り出す！
+                <br />
+                それがTogether Danceというサービスです！
+                <br />
+                こんな人にぴったり
+                <br />
+                好きなダンスを気楽に踊る場が欲しい
+                <br />
+                ダンスを一緒に踊る人が見つけたい
+                <br />
+                自分で作った振り付けを振り入れしたいけど振り入れできる場がない
+                <br />
+                ダンスを教えてみたいけどレッスンを開講する機会はがなかなかない
+                <br />
+                そんな方々に使って欲しいと思っています！ぜひ一度使ってみてください！
+                <br />
+              </h2>
+            </div>
+          </div>
+        </div> */}
+        {/* </div> */}
         <div className="bg-purple-600">
           <div className="container mx-auto">
-            <div className="flex py-16 items-center ">
-              <div className="text-4xl text-white">
+            <div className="flex py-6 md:py-16 items-center justify-center">
+              <div className="text-2xl md:text-4xl text-white ">
                 誰でもイベントを開催できる
               </div>
             </div>
@@ -93,22 +180,22 @@ const About = () => {
           <div className="flex bg-white">
             <div className="flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2">
               <div>
-                <h2 className="text-3xl font-semibold text-gray-800 md:text-4xl">
+                <h2 className="mt-2 text-3xl font-semibold text-gray-800 md:text-4xl">
                   踊ろう会を開催する
-                  <span className="text-indigo-600">気楽に</span>
+                  {/* <span className="text-indigo-600">気楽に</span> */}
                 </h2>
-                <p className="mt-2 text-2xl text-gray-500">
-                  誰でも簡単にイベントを開催することができます
+                <p className="mt-2 text-base md:text-2xl text-gray-500">
+                  誰でも簡単にイベントを開催できます
                   <br />
-                  自分で振り付けしたものをみんなに振り入れして踊る！
+                  自分で振り付けしたダンスをみんなと踊る会！
                   <br />
-                  好きなアーティストのダンスを真似て踊る！
+                  好きなアーティストのダンスを真似て踊る会！
                   <br />
                   などなど自由な形式でOK！！
                 </p>
-                <div className="mt-12">
+                <div className="m-4 md:mt-12">
                   <Link href="newEvent">
-                    <a className="text-3xl text-blue-400 hover:text-blue-500">
+                    <a className="text-lg md:text-3xl text-blue-400 hover:text-blue-500">
                       踊ろう会の開催はこちらから →
                     </a>
                   </Link>
@@ -131,8 +218,8 @@ const About = () => {
         </div>
         <div className="bg-purple-600">
           <div className="container mx-auto">
-            <div className="flex py-16 items-center ">
-              <div className="text-4xl text-white ml-auto">
+            <div className="flex py-6 md:py-16 items-center ">
+              <div className="text-2xl md:text-4xl text-white mx-auto md:ml-auto">
                 いつでも踊ろう会に参加
               </div>
             </div>
@@ -140,8 +227,8 @@ const About = () => {
         </div>
         <div className="w-full">
           <div className="flex bg-white">
-            <div className="hidden lg:block lg:w-1/2 h-3/4">
-              <div className="h-full object-cover">
+            <div className="hidden md:block w-full md:w-1/2 h-full md:h-3/4">
+              <div className="h-full object-cover mx-auto">
                 <div className="h-full bg-black ">
                   {/* eslint-disable-next-line @next/next/no-img-element */}
                   <img
@@ -154,11 +241,11 @@ const About = () => {
             </div>
             <div className="flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2">
               <div>
-                <h2 className="text-3xl font-semibold text-gray-800 md:text-4xl">
-                  好きなダンスをいつでもどこでもだれとでも
-                  <span className="text-indigo-600">一緒に</span>
+                <h2 className="mt-2 text-3xl font-semibold text-gray-800 md:text-4xl">
+                  踊ろう会に自由に参加
+                  {/* <span className="text-indigo-600">一緒に</span> */}
                 </h2>
-                <p className="mt-2 text-2xl text-gray-500">
+                <p className="mt-2 text-base md:text-2xl text-gray-500">
                   好きな踊ろう会にいつでも自由に参加可能！
                   <br />
                   好きなダンスが同じ人同士で一緒に踊ったり
@@ -167,9 +254,9 @@ const About = () => {
                   <br />
                   好きなタイミング好きな目的を持ってみんなで楽しくダンシング！！
                 </p>
-                <div className="mt-12">
+                <div className="m-4 md:mt-12">
                   <Link href="/events">
-                    <a className="text-3xl text-blue-400 hover:text-blue-500">
+                    <a className="text-lg md:text-3xl text-blue-400 hover:text-blue-500">
                       踊ろう会の検索はこちらから →
                     </a>
                   </Link>
