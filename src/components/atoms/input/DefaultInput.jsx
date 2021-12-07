c321f4f82b6e145b1cf2e23ab13aba2eb4b90e70
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-12-07 22:28:49 +0900

    ✨ デフォルト用インプットのコンポーネント作成

---
+++ src/components/atoms/input/DefaultInput.jsx
@@ -0,0 +1,12 @@
+export const DefaultInput = (props) => {
+  const { type, id, placeholder, arial } = props;
+  return (
+    <input
+      className="w-full px-5 py-1 text-gray-700 bg-gray-300 rounded focus:outline-none focus:bg-white"
+      type={type}
+      id={id}
+      placeholder={placeholder}
+      arial-label={arial}
+    />
+  );
+};
