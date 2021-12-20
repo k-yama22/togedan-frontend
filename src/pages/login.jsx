121d25fd5fcdb197dd6f231bfac04c708967cb5b
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-12-20 17:16:42 +0900

    💄 フォームのサイズの共通化

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -23,10 +23,10 @@ const Login = memo(() => {
         <div className="bg-gray-500 flex flex-col items-center justify-center min-h-hull md:min-h-screen py-2">
           <main className="flex flex-col items-center justify-center w-full flex-1 px-10 md:px-20 text-center">
             <div className="container mx-auto h-full flex flex-1 justify-center items-center">
-              <div className="w-full max-w-md">
+              <div className="w-full max-w-2xl">
                 <h1 className="bg-green-100">Together Dance</h1>
                 <div className="leading-loose">
-                  <div className="max-w-md m-4 p-5 md:p-10 bg-white bg-opacity-25 rounded shadow-xl">
+                  <div className="max-w-2xl m-4 p-5 md:p-10 bg-white bg-opacity-25 rounded shadow-xl">
                     <p className="text-white font-medium text-center text-lg font-bold">
                       LOGIN
                     </p>
