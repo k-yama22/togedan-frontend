ab18ef3e69bc7c7b08ffe5a7a5c704efe240b31d
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-12-21 15:48:25 +0900

    ♻️ レイアウトの統一、フォーム用レイアウトのコンポーネント適用

--- src/pages/iconEdit.jsx
+++ src/pages/iconEdit.jsx
@@ -6,6 +6,7 @@ import { useUserChange } from "src/hooks/useUserChange";
 import { Loading } from "src/components/Loading";
 import { useForm } from "react-hook-form";
 import { useMyUserInfo } from "src/hooks/useMyUserInfo";
+import { FormLayout } from "src/components/Layout/FormLayout";
 
 const IconEdit = () => {
   const [image, setImage] = useState("");
@@ -58,58 +59,60 @@ const IconEdit = () => {
       </Head>
 
       <Layout>
-        <div className="bg-gray-500 flex flex-col items-center justify-center min-h-screen py-2">
+        {/* <div className="bg-gray-500 flex flex-col items-center justify-center min-h-screen py-2">
           <main className="flex flex-col items-center justify-center w-full flex-1 px-10 md:px-20 text-center">
             <div className="container mx-auto h-full flex flex-1 justify-center items-center">
-              <div className="w-full max-w-md">
+              <div className="w-full max-w-2xl">
                 <h1 className="bg-green-100">Together Dance</h1>
                 <div className="leading-loose">
-                  <div className="max-w-md m-4 p-8 bg-white bg-opacity-25 rounded shadow-xl mx-auto">
-                    <p className="text-white font-medium text-center text-lg font-bold">
-                      アイコン画像を選択してください
-                    </p>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                      <div className="m-2">
-                        <div className="rounded-lg mx-auto">
-                          <div className="mx-auto p-4">
-                            {preview ? (
-                              // eslint-disable-next-line @next/next/no-img-element
-                              <img
-                                className="mx-auto object-cover lg:h-48 lg:w-48 rounded-2xl h-48 w-48"
-                                src={preview}
-                                alt="preview img"
-                                width="256"
-                                height="256"
-                              />
-                            ) : null}
-                          </div>
-                          <label className="cursor-pointer mt-12">
-                            <div className="mt-2 text-base leading-normal px-4 py-2 bg-teal-400 text-white text-sm rounded-full">
-                              画像を選択する
-                            </div>
-                            <input
-                              id="image"
-                              type="file"
-                              className="hidden"
-                              accept="image/*"
-                              placeholder="アイコン画像"
-                              onChange={onChangeImage}
-                            />
-                          </label>
-                        </div>
-                      </div>
-                      <div className="mt-4 items-center flex justify-between">
-                        <button className="w-48 px-4 py-1 flex items-center justify-center text-white font-light tracking-wider bg-gray-900 hover:bg-gray-800 rounded mx-auto">
-                          {loading ? <Loading /> : <>変更</>}
-                        </button>
-                      </div>
-                    </form>
+                  <div className="max-w-2xl m-4 p-8 bg-white bg-opacity-25 rounded shadow-xl mx-auto"> */}
+        <FormLayout>
+          <p className="text-white font-medium text-center text-lg font-bold">
+            アイコン画像を選択してください
+          </p>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="m-2">
+              <div className="rounded-lg mx-auto">
+                <div className="mx-auto p-4">
+                  {preview ? (
+                    // eslint-disable-next-line @next/next/no-img-element
+                    <img
+                      className="mx-auto object-cover lg:h-48 lg:w-48 rounded-2xl h-48 w-48"
+                      src={preview}
+                      alt="preview img"
+                      width="256"
+                      height="256"
+                    />
+                  ) : null}
+                </div>
+                <label className="cursor-pointer mt-12">
+                  <div className="mt-2 text-base leading-normal px-4 py-2 bg-teal-400 text-white text-sm rounded-full">
+                    画像を選択する
                   </div>
+                  <input
+                    id="image"
+                    type="file"
+                    className="hidden"
+                    accept="image/*"
+                    placeholder="アイコン画像"
+                    onChange={onChangeImage}
+                  />
+                </label>
+              </div>
+            </div>
+            <div className="mt-4 items-center flex justify-between">
+              <button className="w-48 px-4 py-1 flex items-center justify-center text-white font-light tracking-wider bg-gray-900 hover:bg-gray-800 rounded mx-auto">
+                {loading ? <Loading /> : <>変更</>}
+              </button>
+            </div>
+          </form>
+        </FormLayout>
+        {/* </div>
                 </div>
               </div>
             </div>
           </main>
-        </div>
+        </div> */}
       </Layout>
     </>
   );
--- src/pages/passForget.jsx
+++ src/pages/passForget.jsx
@@ -5,6 +5,7 @@ import { Loading } from "src/components/Loading";
 import { useForm } from "react-hook-form";
 import { usePassForget } from "src/hooks/usePassForget";
 import { PASS_RESET_REDIRECT_URL } from "src/utils/constants";
+import { FormLayout } from "src/components/Layout/FormLayout";
 
 const PassForget = () => {
   const { passForget, loading } = usePassForget();
@@ -36,48 +37,50 @@ const PassForget = () => {
       </Head>
 
       <Layout>
-        <div className="bg-gray-500 flex flex-col items-center justify-center min-h-hull md:min-h-screen py-2">
+        {/* <div className="bg-gray-500 flex flex-col items-center justify-center min-h-hull md:min-h-screen py-2">
           <main className="flex flex-col items-center justify-center w-full flex-1 px-10 md:px-20 text-center">
             <div className="container mx-auto h-full flex flex-1 justify-center items-center">
               <div className="w-full max-w-2xl">
                 <h1 className="bg-green-100">Together Dance</h1>
                 <div className="leading-loose">
-                  <div className="max-w-2xl m-4 p-10 bg-white bg-opacity-25 rounded shadow-xl mx-auto">
-                    <p className="text-white font-medium text-center text-md md:text-lg font-bold">
-                      メールアドレスを入力してください
-                    </p>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                      <div className="m-2">
-                        <label
-                          className="block text-xs md:text-sm text-white"
-                          htmlFor="email"
-                        >
-                          メールアドレス
-                        </label>
-                        <input
-                          className="w-full px-5 py-1 text-gray-700 bg-gray-300 rounded focus:outline-none focus:bg-white"
-                          id="email"
-                          type="email"
-                          placeholder="メールアドレス"
-                          {...register("email", { required: true })}
-                        />
-                        {errors.email && errors.email.type === "required" && (
-                          <span className="text-red-700">必須項目です</span>
-                        )}
-                      </div>
+                  <div className="max-w-2xl m-4 p-10 bg-white bg-opacity-25 rounded shadow-xl mx-auto"> */}
+        <FormLayout>
+          <p className="text-white font-medium text-center text-md md:text-lg font-bold">
+            メールアドレスを入力してください
+          </p>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="m-2">
+              <label
+                className="block text-xs md:text-sm text-white"
+                htmlFor="email"
+              >
+                メールアドレス
+              </label>
+              <input
+                className="w-full px-5 py-1 text-gray-700 bg-gray-300 rounded focus:outline-none focus:bg-white"
+                id="email"
+                type="email"
+                placeholder="メールアドレス"
+                {...register("email", { required: true })}
+              />
+              {errors.email && errors.email.type === "required" && (
+                <span className="text-red-700">必須項目です</span>
+              )}
+            </div>
 
-                      <div className="mt-4 items-center flex justify-between">
-                        <button className="w-48 px-4 py-1 flex items-center justify-center text-white font-light tracking-wider bg-gray-900 hover:bg-gray-800 rounded m-auto">
-                          {loading ? <Loading /> : <>リセット</>}
-                        </button>
-                      </div>
-                    </form>
-                  </div>
+            <div className="mt-4 items-center flex justify-between">
+              <button className="w-48 px-4 py-1 flex items-center justify-center text-white font-light tracking-wider bg-gray-900 hover:bg-gray-800 rounded m-auto">
+                {loading ? <Loading /> : <>リセット</>}
+              </button>
+            </div>
+          </form>
+          {/* </div>
                 </div>
               </div>
             </div>
           </main>
-        </div>
+        </div> */}
+        </FormLayout>
       </Layout>
     </>
   );
